88e339ca20bc624d2fe87b1e8c917633887a124a
Lucas Lazzaretti Weber <lucas.weber@ska.com.br>
2023-10-24 12:38:06 -0300

    Accordion transformado em Class

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import ScrollSuave from "./modules/scroll-suave.js";
+import Accordion from "./modules/accordion.js";
 import initModal from "./modules/modal.js"
 import initToolTip from "./modules/tooltip.js"
 import initDropDownMenu from "./modules/dropdown-menu.js"
@@ -6,15 +7,15 @@ import initMenuMobile from "./modules/menu-mobile.js"
 import initFuncionamento from "./modules/funcionamento.js"
 import initFetchAnimais from "./modules/fetch-animais.js"
 import initFetchBitcoin from "./modules/bitcoin.js"
-import { initAccordion } from "./modules/accordion.js";
 import { initTabNav } from "./modules/tabNavigation.js";
 import initAnimacaoScroll from "./modules/anima-scroll.js";
 
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 
-initAccordion();
 initTabNav();
 initModal();
 initToolTip();
