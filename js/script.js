0ab188d7092ec5081ddba65f2083284108d0c78f
Lucas Lazzaretti Weber <lucas.weber@ska.com.br>
2023-10-24 16:53:03 -0300

    modal refatorado com Class

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
-import initModal from "./modules/modal.js"
+import TabNav from "./modules/tabNavigation.js";
+import Modal from "./modules/modal.js"
 import initToolTip from "./modules/tooltip.js"
 import initDropDownMenu from "./modules/dropdown-menu.js"
 import initMenuMobile from "./modules/menu-mobile.js"
 import initFuncionamento from "./modules/funcionamento.js"
 import initFetchAnimais from "./modules/fetch-animais.js"
 import initFetchBitcoin from "./modules/bitcoin.js"
-import TabNav from "./modules/tabNavigation.js";
 import initAnimacaoScroll from "./modules/anima-scroll.js";
 
 
@@ -17,8 +17,9 @@ const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
 
-initModal();
 initToolTip();
 initDropDownMenu();
 initMenuMobile();
